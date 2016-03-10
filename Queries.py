f8e2d020317deffb655af3ea0f91a6cdae08004b
Katherine Burckin <kburckin@pc12.cs.ucdavis.edu>
2016-03-09 21:40:42 -0800

    Working query for part B, still need to convert to use psycopg2 and a loop

--- Queries.py
+++ Queries.py
@@ -13,3 +13,18 @@ query1 =
 
 #for i in (5,10,15,...)
     cur.execute(query1);
+
+
+
+#b) Calculate the average fuel economy of all miles traveled for trips less than specific distances from previous problem. Only consider trips that utilize a household vehicle (VEHID is 1 or larger), use the EPA combined fuel economy (EPATMPG) for the particular vehicle.
+
+SELECT SUM(totalmilespervehicle * "EPATMPG") / SUM(totalmilespervehicle)
+FROM (SELECT daytrip."HOUSEID", daytrip."VEHID", SUM("TRPMILES") AS totalmilespervehicle, "EPATMPG"
+FROM daytrip, vehicle
+WHERE daytrip."HOUSEID" = vehicle."HOUSEID" AND daytrip."VEHID" = vehicle."VEHID" AND "TRPMILES" >= 0 AND "TRPMILES" < 5
+GROUP BY daytrip."HOUSEID", daytrip."VEHID", "EPATMPG"
+HAVING daytrip."VEHID" >= 1) X; #might want to change this to test "TRPHHVEH" = '01'
+
+for i in (5,10,15,...)
+    cur.execute(queryB)
+    answer[i] = cur.fetchone()
