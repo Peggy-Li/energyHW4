90578cbbde4436cdec2f27959b48d75c54feea31
Katherine Burckin <kburckin@pc12.cs.ucdavis.edu>
2016-03-09 22:03:46 -0800

    Part B should be complete

--- Queries.py
+++ Queries.py
@@ -1,20 +1,11 @@
+import os
 import psycopg2
 
 
 conn = psycopg2.connect("dbname=postgres host=/home/" + os.environ['USER'] + "/postgres")
 cur = conn.cursor()
 
-#Calculate the percent of individuals that travel less than 5 - 100 miles a day for every 5 mile increments (e.g. 5, 10, 15, ..., 95, 100).
-
-
-
-
-query1 = 
-
-#for i in (5,10,15,...)
-    cur.execute(query1);
-
-
+#a) Calculate the percent of individuals that travel less than 5 - 100 miles a day for every 5 mile increments (e.g. 5, 10, 15, ..., 95, 100).
 
 #b) Calculate the average fuel economy of all miles traveled for trips less than specific distances from previous problem. Only consider trips that utilize a household vehicle (VEHID is 1 or larger), use the EPA combined fuel economy (EPATMPG) for the particular vehicle.
 
@@ -24,12 +15,20 @@ FROM (SELECT daytrip."HOUSEID", daytrip."VEHID", SUM("TRPMILES") AS totalmilespe
 FROM daytrip, vehicle
 WHERE daytrip."HOUSEID" = vehicle."HOUSEID" AND daytrip."VEHID" = vehicle."VEHID" AND "TRPMILES" >= 0 AND "TRPMILES" < %d
 GROUP BY daytrip."HOUSEID", daytrip."VEHID", "EPATMPG"
-HAVING daytrip."VEHID" >= 1) X; #might want to change this to test "TRPHHVEH" = '01'
+HAVING daytrip."VEHID" >= 1) X;
 """
+
 answer = []
-for i in range(5, 101, 5)
-    cur.execute(queryB % i)
-    answer[i] = cur.fetchone()
+for upper_limit in range(5, 101, 5):
+    cur.execute(queryB % upper_limit)
+    answer.append(cur.fetchone()) 
+
+print answer
+
+
+
+
+
 conn.commit()
 cur.close()
 conn.close()
